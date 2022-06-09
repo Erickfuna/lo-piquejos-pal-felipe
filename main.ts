754fef954f1d87b5f63c0bfd623300ea75e14f80
Erickfuna <107216829+Erickfuna@users.noreply.github.com>
2022-06-09 22:41:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const Vida = SpriteKind.create()
+    export const Munició = SpriteKind.create()
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (info.score() > 0) {
         projectile = sprites.createProjectileFromSprite(img`
@@ -21,12 +25,23 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         music.pewPew.play()
         info.changeScoreBy(-1)
     } else {
-        Nau.sayText("No em queda munició!", 2000, false)
+        Nau.sayText("No em Queda munició!", 2000, false)
+        music.sonar.play()
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Munició, function (sprite, otherSprite) {
+    info.changeScoreBy(2)
+    otherSprite.destroy()
+    music.powerUp.play()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Vida, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    otherSprite.destroy()
+    music.beamUp.play()
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     otherSprite.destroy(effects.confetti, 500)
-    info.changeScoreBy(1)
+    info.changeScoreBy(2)
     music.smallCrash.play()
     sprite.destroy()
 })
@@ -36,10 +51,13 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     scene.cameraShake(4, 500)
     music.bigCrash.play()
 })
+let Recarga_munició: Sprite = null
 let Asteroide: Sprite = null
+let Vides: Sprite = null
 let projectile: Sprite = null
 let Nau: Sprite = null
-game.splash("EL PLANETA EXPLOTAT", "Prem A per començar i B per disparar")
+game.splash("ELIMINA ALS ASTERIODES", "Prem A per començar i B per disparar")
+game.splash("Recull les monedes per guanyar munició i els cors per guanyar vides")
 effects.starField.startScreenEffect()
 Nau = sprites.create(img`
     ................
@@ -84,6 +102,28 @@ controller.moveSprite(Nau, 100, 100)
 Nau.setStayInScreen(true)
 info.setLife(5)
 info.setScore(5)
+game.onUpdateInterval(33000, function () {
+    Vides = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . f f f f f f . f f f f f f . 
+        . f f 3 3 3 3 f f f 3 3 3 3 f f 
+        . f 3 3 3 3 3 3 f 3 3 3 3 3 3 f 
+        . f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+        . f 3 3 3 3 3 3 3 3 1 1 1 3 3 f 
+        . f 3 3 3 3 3 b b b 1 1 1 3 3 f 
+        . f 3 3 3 3 b b b b b 3 3 3 3 f 
+        . f f 3 3 b b b b b b b 3 3 f f 
+        . . f f 3 b b b b b b b 3 f f . 
+        . . . f f b b b b b b b f f . . 
+        . . . . f f b b b b b f f . . . 
+        . . . . . f f b b b f f . . . . 
+        . . . . . . f f b f f . . . . . 
+        . . . . . . . f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, 0, 50)
+    Vides.x += randint(0, scene.screenWidth())
+    Vides.setKind(SpriteKind.Vida)
+})
 game.onUpdateInterval(1000, function () {
     Asteroide = sprites.createProjectileFromSide(img`
         . . . . . . . . . c c 8 . . . . 
@@ -105,4 +145,26 @@ game.onUpdateInterval(1000, function () {
         `, 0, 50)
     Asteroide.x += randint(0, scene.screenWidth())
     Asteroide.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(20000, function () {
+    Recarga_munició = sprites.createProjectileFromSide(img`
+        . . . . b b b b b b b b . . . . 
+        . . . . b b b b b b b b . . . . 
+        . . b b 5 5 5 5 5 5 5 5 b b . . 
+        . . b b 5 5 5 5 5 5 5 5 b b . . 
+        b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+        b b 5 5 d d 3 3 3 3 d d 5 5 b b 
+        b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+        b b 5 5 3 3 5 5 5 5 1 1 5 5 b b 
+        c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+        c c 5 5 3 3 5 5 5 5 1 1 d d c c 
+        c c d d d d 1 1 1 1 d d d d c c 
+        c c d d d d 1 1 1 1 d d d d c c 
+        . . f f d d d d d d d d f f . . 
+        . . f f d d d d d d d d f f . . 
+        . . . . f f f f f f f f . . . . 
+        . . . . f f f f f f f f . . . . 
+        `, 0, 50)
+    Recarga_munició.x += randint(0, scene.screenWidth())
+    Recarga_munició.setKind(SpriteKind.Munició)
 })
